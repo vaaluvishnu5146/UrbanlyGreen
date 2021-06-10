54f75ff344e8dbe8f47b81953c2d83a9fa8b9dd6
vishnu vardhan <vaaluvishnu5146@gmail.com>
2021-06-10 22:37:38 +0530

    NavBar component and styling and Dashboard page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,53 @@
-import { IonContent, IonPage, IonLabel } from "@ionic/react";
-import "./Home.css";
+import {
+  IonContent,
+  IonPage,
+  IonLabel,
+  IonCard,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/react";
+import "./Home.scss";
+//COMPONENTS
+import NavBar from "../../components/NavBar/NavBar";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <IonLabel>Home Page</IonLabel>
+        <div className="pageContainer">
+          <NavBar />
+          <IonLabel className="pageHeading">Quick Insights</IonLabel>
+          {/* <IonCard>Today's Insights</IonCard> */}
+          <IonGrid>
+            <IonRow>
+              <IonCol sizeLg="4" sizeMd="4" sizeXs="6">
+                <IonCard className="analyticsCard">
+                  <IonLabel className="">Total Listings</IonLabel>
+                  <IonLabel className="numbers">10</IonLabel>
+                </IonCard>
+              </IonCol>
+              <IonCol sizeLg="4" sizeMd="4" sizeXs="6">
+                <IonCard className="analyticsCard">
+                  <IonLabel className="">Total Earnings</IonLabel>
+                  <IonLabel className="numbers">30,000</IonLabel>
+                </IonCard>
+              </IonCol>
+              <IonCol sizeLg="4" sizeMd="4" sizeXs="6">
+                <IonCard className="analyticsCard">
+                  <IonLabel className="">Total Orders</IonLabel>
+                  <IonLabel className="numbers">100</IonLabel>
+                </IonCard>
+              </IonCol>
+              <IonCol sizeLg="4" sizeMd="4" sizeXs="6">
+                <IonCard className="analyticsCard">
+                  <IonLabel className="">Total Order Cancelations</IonLabel>
+                  <IonLabel className="numbers">20</IonLabel>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </div>
       </IonContent>
     </IonPage>
   );
